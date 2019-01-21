94c90638927cad07b05fe35e99dbe71320613272
Muna <mmhussein125@gmail.com>
2019-01-21 22:09:07 +0000

    Adds count property

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0
+    };
+
     render() { 
        return (
         <React.Fragment>
-           <h1>Hello World!</h1>
-           <button>Increment</button>
+           <span>{this.state.count}</span>
+           <button>+</button>
         </React.Fragment>
        );
     }
