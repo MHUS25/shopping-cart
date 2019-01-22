de73f32244eb9a5749710f0f6fc428aa0b9b1876
Muna <mmhussein125@gmail.com>
2019-01-22 00:31:33 +0000

    Renders class dynamicaly

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,9 +10,14 @@ class Counter extends Component {
     };
 
     render() { 
+
+        let classes = "badge m-2 badge-";
+
+        classes += this.state.count === 0 ? "warning" : "primary";
+
        return (
         <React.Fragment>
-           <span style={this.styles} className="badge badge-primary m-2">{this.state.count}</span>
+           <span style={this.styles} className={classes}>{this.state.count}</span>
            <button className="btm btn-secondary btn-sm">+</button>
         </React.Fragment>
        );
