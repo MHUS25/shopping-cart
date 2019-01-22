74b912afa9c8871793cfd2136f2ad12ae38adc0b
Muna <mmhussein125@gmail.com>
2019-01-22 00:41:12 +0000

    Refactors by adding getBadgeClasses method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,16 +11,18 @@ class Counter extends Component {
 
     render() { 
 
-        let classes = "badge m-2 badge-";
-
-        classes += this.state.count === 0 ? "warning" : "primary";
-
        return (
         <React.Fragment>
-           <span style={this.styles} className={classes}>{this.formatCount()}</span>
+           <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
            <button className="btm btn-secondary btn-sm">+</button>
         </React.Fragment>
        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
