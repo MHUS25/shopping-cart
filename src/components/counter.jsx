fd1cc57d47956485a5c11b286c4878ee3a94c896
Muna <mmhussein125@gmail.com>
2019-01-22 21:51:22 +0000

    Renders basket items dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        items: ["handbag", "shoes"]
     };
 
     styles = {
@@ -12,10 +13,11 @@ class Counter extends Component {
     render() { 
 
        return (
-        <React.Fragment>
+        <div>
            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
            <button className="btm btn-secondary btn-sm">+</button>
-        </React.Fragment>
+           <ul>{this.state.items.map(item => <li>{ item }</li>)}</ul>
+        </div>
        );
     }
 
