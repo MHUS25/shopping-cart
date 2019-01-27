d3875d3c907a9cadd4329fb6c899df6fb7c17013
Muna <mmhussein125@gmail.com>
2019-01-27 18:21:01 +0000

    Removes local state of counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,37 +1,30 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value
-    };
 
     styles = {
         fontSize: 15
     };
-
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 })
-    }
 
     render() {
        return (
         <div>
           <h4>Item {this.props.id}</h4>
            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-           <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">+</button>
-           <button onClick={ () => this.props.onDelete(this.props.counter .id) } className="btn btn-danger btn-sm m-2">Delete</button>
+           <button onClick={ () => this.props.onIncrement(this.props.counter) } className="btn btn-secondary btn-sm">+</button>
+           <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
        );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {value} = this.state
+        const {value} = this.props.counter
         return (value === 0 ? "Zero" : value);
     }
 
