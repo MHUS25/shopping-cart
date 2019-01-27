7d33b1ab2e7e0d382751c01a099edab5510b520a
Muna <mmhussein125@gmail.com>
2019-01-27 18:01:27 +0000

    Adds reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -18,7 +18,9 @@ class Counters extends Component {
   }
 
   render() {
-    return(<div>
+    return(
+      <div>
+      <button className="btn btn-primary btn-sm m-2">Reset</button>
       { this.state.counters.map(counter => (
         <Counter
         key={counter.id}
