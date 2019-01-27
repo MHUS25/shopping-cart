c9e1a1c61119b13de4dcb3c27aac2f94ccc10c22
Muna <mmhussein125@gmail.com>
2019-01-27 17:35:30 +0000

    Passes counter object instead of multiple props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.value
     };
 
     styles = {
@@ -19,7 +19,7 @@ class Counter extends Component {
           <h4>Item {this.props.id}</h4>
            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
            <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">+</button>
-           <button onClick={ () => this.props.onDelete(this.props.id) } className="btn btn-danger btn-sm m-2">Delete</button>
+           <button onClick={ () => this.props.onDelete(this.props.counter .id) } className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
        );
     }
