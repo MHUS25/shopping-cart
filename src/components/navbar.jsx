24cdf63253f437976784c791299836972479b3f5
Muna <mmhussein125@gmail.com>
2019-01-27 22:48:05 +0000

    Adds props as a parameter

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 //Stateless functional component
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Items in basket{" "}
         <span className="badge badge-pill badge-secondary">
-          {this.props.totalCounters}
+          {props.totalCounters}
         </span>
       </a>
     </nav>
