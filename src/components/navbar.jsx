daad7c1edb1dcda1e830d9e2e87262a9f1f59576
Muna <mmhussein125@gmail.com>
2019-01-27 22:51:07 +0000

    Refactors with object destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 //Stateless functional component
 
-const Navbar = (props) => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Items in basket{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
