df7f80213b66796b07a1c6b0a6004ed12b74e9c4
Muna <mmhussein125@gmail.com>
2019-01-28 01:18:59 +0000

    Playing with lifecycle hooks - updating phase

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
+    componentDidUpdate(prevProps, prevState) {
+      console.log("prevProps", prevProps);
+      console.log("prevState", prevState);
+      if (prevProps.counter.value !== this.props.counter.value)
+      //Ajax call & get new data from server
+    }
+
     styles = {
         fontSize: 15
     };
